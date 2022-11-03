7f0bb664bc9bc19a65750c54c914a83243827e82
fayoo19 <fayazaly19@gmail.com>
2022-11-03 10:31:16 -0700

    stats

---
+++ app.js
@@ -0,0 +1,71 @@
+// IF STATEMENT
+
+// let marks = prompt("ENTER A MARKS")
+// if(marks>=90 && marks<=100){
+//    alert ("A++")
+// }
+// else if (marks>=80 && marks<=90){
+// alert("A+")
+// }
+// else if (marks>=70 && marks<=80){
+//   alert("A")
+//     }
+//  else if (marks>=60 && marks<=70){
+//  alert("B")
+//           }
+//  else if (marks>=50 && marks<=60){
+// alert("C")
+//               }
+//  else if (marks>=0 && marks<=39){
+//  alert("FAIL")
+//                   }
+//  else{ alert("invalid entry")}
+
+// let temp = prompt("ENTER A TEMP")
+// if(temp>=45 && temp<=50){
+//    alert ("TEMP IS VERY HOT")
+// }
+// // else if (marks>=30 && marks<=45){
+// // alert("temp is hot")
+// // }
+// // else if (marks>=20 && marks<=30){
+// //   alert("temp is normal")
+// //     }
+// //  else if (marks>=10 && marks<=20){
+// //  alert("temp is cold")
+// //           }
+// //  else if (marks>=5 && marks<=10){
+// // alert(" temp is very cold")
+// //               }
+// //  else if (marks>=-5 && marks<=0){
+// //  alert("less then 0")
+// //                   }
+//  else{ alert("invalid entry")}
+//  SWITCH STATEMENT
+
+// let day =  prompt("enter a day")
+
+// switch (day){
+//     case "1":
+//         alert("sunday")
+//         break;
+//         case "2":
+//             alert ("monday")
+//             break;
+//             case "3":
+//               alert ("tuesday")
+//                 break;
+//                 case "4":
+//                    alert ("wednesday")
+//                     break;
+//                    case "5":
+//                        alert ("thusday")
+//                         break;
+//                         case "6":
+//                            alert ("friday")
+//                             break;
+//                             case "7":
+//                             alert("sturday")
+//                                 break;
+//                               default:
+//                                 alert("invalid entry")
